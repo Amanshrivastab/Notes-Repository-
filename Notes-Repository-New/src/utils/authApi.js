68e5ecbc22a4d186251419095c52ba290acbcf36
Aman shrivastab <akshri2007@gmail.com>
2026-08-23 01:28:34 +0530

    Add utils for authentication

--- Notes-Repository-New/src/utils/authApi.js
+++ Notes-Repository-New/src/utils/authApi.js
@@ -25,3 +25,28 @@ export const loginUser = async (email, password) => {
 
     return data;
 };
+
+export const registerUser = async (name, email, password) => {
+    const response = await fetch(
+        "http://localhost:5000/api/auth/register",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                name,
+                email,
+                password
+            })
+        }
+    );
+
+    const data = await response.json();
+
+    if (!response.ok) {
+        throw new Error(data.message || "Registration failed");
+    }
+
+    return data;
+};
